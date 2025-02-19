aadf66293fdc7d2ede2292d7b1cec882da4df4ab
swarup Goswami <goswamiswarup97@gmail.com>
2025-02-19 12:52:26 +0530

    updated the middlware

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -13,13 +13,10 @@ module.exports.authUser=async(req,res,next)=>{
 
     if(isBlacklisted){
         return res.status(401).json({message:'unauthorized'});
+        
     }
 
     
-
-
-
-
     try{
         const decoded=jwt.verify(token,process.env.JWT_SECRET);
         const user=await userModel.findById(decoded._id);
@@ -37,7 +34,7 @@ module.exports.authUser=async(req,res,next)=>{
 
 module.exports.authCaptain=async(req,res,next)=>{
 
-    const token=req.cookies.token || req.headers.authorization ?.split(' ')[1];
+    const token=req.cookies.token || req.headers.authorization?.split(' ')[1];
     if(!token){
         return res.status(401).json({message:'unauthorized'});
     }
